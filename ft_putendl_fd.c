be393c0c67d54adf30343862c03e29f597de3fe4
Wassim Daoudi Idrissi <wdaoudi-@bess-f1r3s14.clusters.42paris.fr>
2024-05-27 21:04:33 +0200

    kk

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 19:45:46 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/05/27 21:00:09 by wdaoudi-         ###   ########.fr       */
+/*   Created: 2024/05/27 20:51:06 by wdaoudi-          #+#    #+#             */
+/*   Updated: 2024/05/27 21:00:25 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void ft_putendl_fd(char *s, int fd)
 {
-	size_t i;
+    size_t i;
 
 	i = 0;
 	while (s[i])
@@ -22,4 +22,12 @@ void	ft_putstr_fd(char *s, int fd)
 		write(fd, &s[i], 1);
 		i++;
 	}
+    write(fd, "\n", 1);
 }
+/*int main (int ac, char **av)
+{
+    if(ac == 0)
+        return 0;
+    ft_putendl_fd(av[1], 0);
+    return 0;
+}*/
