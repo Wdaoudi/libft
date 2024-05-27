85fd05e49a0e06e4a9d7321be8ee4b9358987ce9
Wassim Daoudi Idrissi <wdaoudi-@paul-f5Br6s2.clusters.42paris.fr>
2024-05-27 18:39:03 +0200

    kk

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,51 +1,62 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/27 15:59:28 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/05/27 16:48:46 by wdaoudi-         ###   ########.fr       */
+/*   Created: 2024/05/27 16:49:18 by wdaoudi-          #+#    #+#             */
+/*   Updated: 2024/05/27 18:38:46 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s);
-
-char	*ft_strjoin(char const *s1, char const *s2)
+static int	isset(const char s, const char *set)
 {
-	size_t	i;
-	size_t	k;
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (s == set[i])
+			return (1);
+		else
+			i++;
+	}
+	return (0);
+}
+char	*ft_strtrim(const char *s1, const char *set)
+{
 	char	*str;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
 	k = 0;
 	i = 0;
-	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
+	if (ft_strlen(s1) == 0)
+		return (ft_strdup(""));
+	while (isset(s1[i], set) == 1)
+		i++;
+	j = ft_strlen(s1) - 1;
+	while (isset(s1[j], set) == 1)
+		j--;
+	str = malloc(sizeof(char) * (j - i + 1));
+	if (str == NULL)
 		return (NULL);
-	while (s1[i])
+	while (i <= j)
 	{
 		str[k] = s1[i];
 		k++;
 		i++;
 	}
-	i = 0;
-	while (s2[i])
-	{
-		str[k] = s2[i];
-		k++;
-		i++;
-	}
-	str[k] = 0;
 	return (str);
 }
-
 /*int	main(int ac, char **av)
 {
 	if (ac == 0)
 		return (0);
-	printf("%s", ft_strjoin(av[1], av[2]));
+	printf("%s\n", ft_strtrim(av[1],av[2]));
 	return (0);
 }*/
