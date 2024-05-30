b7d8acb1b71e177f849ab727c734719e9dcd7cfc
Wassim Daoudi Idrissi <wdaoudi-@bess-f2r3s17.clusters.42paris.fr>
2024-05-30 21:35:25 +0200

    kk

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,15 +6,15 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 14:59:23 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2024/05/30 18:25:19 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2024/05/30 21:34:10 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	taille(int n)
+static int	taille(int n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	if (n < 0)
@@ -22,7 +22,9 @@ int	taille(int n)
 		i++;
 		n = -n;
 	}
-	while (n / 10 != 0)
+	if (n == 0)
+		i++;
+	while (n != 0)
 	{
 		i++;
 		n = n / 10;
@@ -30,65 +32,37 @@ int	taille(int n)
 	return (i);
 }
 
-char	*zero(void)
-{
-	char	*str;
-
-	str = malloc(sizeof(char) * 2);
-	str[0] = 48;
-	str[1] = 0;
-	return (str);
-}
-
-char	*int_min(void)
-{
-	char	*temp;
-	char	*str;
-	int		i;
-
-	i = 0;
-	temp = "-2147483648";
-	str = malloc(sizeof(char) * 12);
-	while (temp[i])
-	{
-		str[i] = temp[i];
-		i++;
-	}
-	str[i] = 0;
-	return (str);
-}
-
 char	*ft_itoa(int n)
 {
-	int		i;
+	size_t	i;
 	char	*str;
+	long	nbr;
 
-	i = taille(n);
-	if (n == 0)
-		return (zero());
-	if (n == -2147483648)
-		return (int_min());
-	str = malloc(sizeof(char) * (i + 2));
-	if (!(str))
+	nbr = n;
+	i = taille(nbr);
+	str = malloc((i + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
-	str[i + 1] = '\0';
-	if (n < 0)
+	str[i] = '\0';
+	i--;
+	if (nbr == 0)
+		str[0] = '0';
+	if (nbr < 0)
 	{
-		n = -n;
+		nbr = -nbr;
 		str[0] = '-';
 	}
-	while (n % 10 != 0 || n / 10 != 0)
+	while (nbr != 0)
 	{
-		str[i] = n % 10 + '0';
-		i--;
-		n = n / 10;
+		str[i--] = nbr % 10 + '0';
+		nbr = nbr / 10;
 	}
 	return (str);
 }
 
 // int	main(void)
 // {
-// 	int numbers[] = {0LL, -2147483648LL};
+// 	int numbers[] = {0, -9, -5, 00, 51, -2147483648};
 // 	size_t num_count = sizeof(numbers) / sizeof(numbers[0]);
 
 // 	for (size_t i = 0; i < num_count; i++)
